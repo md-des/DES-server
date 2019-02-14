8c2ff11090f3a7377e6e2b4273c7d8c0e5eee11a
zlx <zlx@omk.io>
2019-02-14 17:02:46 +0800

    add groups detail

--- api/actions/api/groups/index.js
+++ api/actions/api/groups/index.js
@@ -2,4 +2,5 @@ import "../../models/Signup";
 
 export create from "./create";
 export list from "./list";
+export detail from "./detail";
 
--- api/actions/api/groups/list.js
+++ api/actions/api/groups/list.js
@@ -5,6 +5,27 @@ const User = mongoose.model('Signup');
 const Group = mongoose.model("Groups");
 const Post = mongoose.model('Post');
 // const Users = mongoose.model("Signup")
+export const getMemberAndPostsDetails = async function(groups) {
+  const newGroups = [];
+  // 将members和posts通过id查出其对应的详情
+  for (let j = 0; j < groups.length; j++) {
+    const members = groups[j].members;
+    const posts = groups[j].posts;
+    const group = groups[j];
+    group.members = [];
+    group.posts = [];
+    for (let i = 0; i < members.length; i++) {
+      const member = await User.findOne({_id: members[i]}, {_id: 1, name: 1});
+      group.members.push(member)
+    }
+    for (let i = 0; i < posts.length; i++) {
+      const post = await Post.findOne({_id: posts[i]}, {_id: 1, title: 1});
+      group.posts.push(post)
+    }
+    newGroups.push(group)
+  }
+  return newGroups;
+}
 export default async req => {
   const args = await argsFilter(req.query, {
     creator_id: ["required", "string"]
@@ -14,28 +35,9 @@ export default async req => {
   // 参与的groups
   const participantGroups = await Group.find({members: {$in: [args.creator_id]}}).sort({create_time: -1});
   
-  const getMemberNames = async function(groups) {
-    const newGroups = []
-    for (let j = 0; j < groups.length; j++) {
-      const members = groups[j].members;
-      const posts = groups[j].posts;
-      const group = groups[j];
-      group.members = [];
-      group.posts = [];
-      for (let i = 0; i < members.length; i++) {
-        const member = await User.findOne({_id: members[i]}, {_id: 1, name: 1});
-        group.members.push(member)
-      }
-      for (let i = 0; i < posts.length; i++) {
-        const post = await Post.findOne({_id: posts[i]}, {_id: 1, title: 1});
-        group.posts.push(post)
-      }
-      newGroups.push(group)
-    }
-    return newGroups
-  }
-  const myGroupsNew = await getMemberNames(myGroups)
-  const participantGroupsNew = await getMemberNames(participantGroups)
+ 
+  const myGroupsNew = await getMemberAndPostsDetails(myGroups)
+  const participantGroupsNew = await getMemberAndPostsDetails(participantGroups)
   return {
     code: code.success,
     data: {
