e6cd752bce94ceddb868f92ea3318b0855571812
zlx <zlx@omk.io>
2019-03-05 16:43:54 +0800

    delete & rename

--- api/actions/api/groups/index.js
+++ api/actions/api/groups/index.js
@@ -3,4 +3,6 @@ import "../../models/Signup";
 export create from "./create";
 export list from "./list";
 export detail from "./detail";
+export remove from "./delete";
+export rename from "./rename";
 
